e1a55017aa31bca8999f96969e90a134c0c19191
AndresRinconL <darincon126@misena.edu.co>
2020-06-18 17:18:33 -0500

    docs: Actualizacion

---
+++ Desarrollo/PHP/prueba2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Titulo de la pagina</title>
+<metacharset="UTF-8">
+</head>
+<body>
+<h1>Hola ADSI</h1>
+<?php
+    $dia1 = 9;
+    $dia2 = 10;
+    echo "El valor de la primer variable es:";
+    echo $dia1;
+    echo "<br>";
+    echo "El valor de la segunda variable es:";
+    echo $dia2;
+    ?>
+
+
+</body>
+</html>
